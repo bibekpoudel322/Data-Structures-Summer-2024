781f6e7b664d01aa07e402befd1769d351a476ee
BibekPoudel2376 <bp2376@nyu.edu>
2024-05-26 17:35:08 +0400

    g

--- Assignment1/Assignment_1_bp2376.cpp
+++ Assignment1/Assignment_1_bp2376.cpp
@@ -1,67 +1,68 @@
 /**
  * @file:
- * @author 
+ * @author
  * @description Doubly LinkedList implementation in C++
  * @date 2023-03-22
  */
-//startercode_begins
-#include<iostream>
-#include<string>
-#include<sstream>
-#include<iomanip>
+// startercode_begins
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
 using namespace std;
-
 
 class Node
 {
-	private:
-		string elem; //data element (type string in this case)
-		Node* next; //Link (pointer) to the next Node
-		Node* prev; //Link (pointer) to the previous Node
-		friend class DoublyLinkedList;
-		
-	public:
-		Node(): next(NULL), prev(NULL)
-		{}
-		Node(string elem) : elem(elem),next(NULL), prev(NULL)
-		{}
+private:
+	string elem; // data element (type string in this case)
+	Node *next;	 // Link (pointer) to the next Node
+	Node *prev;	 // Link (pointer) to the previous Node
+	friend class DoublyLinkedList;
+
+public:
+	Node() : next(NULL), prev(NULL)
+	{
+	}
+	Node(string elem) : elem(elem), next(NULL), prev(NULL)
+	{
+	}
 };
 //==============================================================
 class DoublyLinkedList
 {
-	private:
-		Node* head; 	// pointer to the head of List
-		Node* tail; 	// pointer to the tail of List
-	public:
-		DoublyLinkedList (); // constructor to create dummy head and tail nodes
-		~DoublyLinkedList (); // destructor to clean up all nodes
-		bool isEmpty() const; // return true if the list is empty
-		const string& front() const; // returns the const reference of the element from the first valid Node of list, throw an exception if the list is empty
-		const string& back() const;  // returns the const reference of the element from last valid Node of the list, throw an exception if the list is empty
-		void addFront(const string& elem); // add a new node to the front of list (after head)
-		void addBack(const string & elem); //add a new node to the back of the list (before tail)
-		void removeFront(); // remove front node from list, throw an exception if the list is empty
-		void removeBack();  // remove last node from list, throw an exception if the list is empty
-		void reverseList();	// reverse the list
-		bool isPalindrome() const; //return true if the list is Palindrome, false otherwise
-		void display() const;	// display all element of the list
+private:
+	Node *head; // pointer to the head of List
+	Node *tail; // pointer to the tail of List
+public:
+	DoublyLinkedList();				   // constructor to create dummy head and tail nodes
+	~DoublyLinkedList();			   // destructor to clean up all nodes
+	bool isEmpty() const;			   // return true if the list is empty
+	const string &front() const;	   // returns the const reference of the element from the first valid Node of list, throw an exception if the list is empty
+	const string &back() const;		   // returns the const reference of the element from last valid Node of the list, throw an exception if the list is empty
+	void addFront(const string &elem); // add a new node to the front of list (after head)
+	void addBack(const string &elem);  // add a new node to the back of the list (before tail)
+	void removeFront();				   // remove front node from list, throw an exception if the list is empty
+	void removeBack();				   // remove last node from list, throw an exception if the list is empty
+	void reverseList();				   // reverse the list
+	bool isPalindrome() const;		   // return true if the list is Palindrome, false otherwise
+	void display() const;			   // display all element of the list
 };
 //===================================
 void listCommands()
 {
-	cout<<"List of available Commands:"<<endl
-		<<"display            : Display the Doubly Linked List"<<endl
-		<<"front              : Display the first element of the List"<<endl
-		<<"back               : Display the last element of the List"<<endl
-		<<"addFront <elem>    : Add a new Node at the beginning of the List"<<endl
-		<<"addBack  <elem>    : Add a new Node at the end of the List"<<endl
-		<<"removeFront        : Remove the first Node of the Linked List"<<endl
-		<<"removeBack         : Remove the last Node of the Linked List"<<endl	
-		<<"isEmpty            : Check if the List is empty?"<<endl
-		<<"reverse            : Reverse the Linked List"<<endl
-		<<"isPalindrome       : Check if the List is Palindrome or not?"<<endl
-		<<"help               : Display the list of available commands"<<endl
-		<<"exit               : Exit the Program"<<endl;
+	cout << "List of available Commands:" << endl
+		 << "display            : Display the Doubly Linked List" << endl
+		 << "front              : Display the first element of the List" << endl
+		 << "back               : Display the last element of the List" << endl
+		 << "addFront <elem>    : Add a new Node at the beginning of the List" << endl
+		 << "addBack  <elem>    : Add a new Node at the end of the List" << endl
+		 << "removeFront        : Remove the first Node of the Linked List" << endl
+		 << "removeBack         : Remove the last Node of the Linked List" << endl
+		 << "isEmpty            : Check if the List is empty?" << endl
+		 << "reverse            : Reverse the Linked List" << endl
+		 << "isPalindrome       : Check if the List is Palindrome or not?" << endl
+		 << "help               : Display the list of available commands" << endl
+		 << "exit               : Exit the Program" << endl;
 }
 //===================================
 int main()
@@ -72,39 +73,51 @@ int main()
 	string command;
 	string parameter;
 
-
 	do
 	{
-		cout<<">";
-		getline(cin,user_input);
+		cout << ">";
+		getline(cin, user_input);
 
 		// parse user-input into command and parameter(s)
 		stringstream sstr(user_input);
-		getline(sstr,command,' ');
-		getline(sstr,parameter);
+		getline(sstr, command, ' ');
+		getline(sstr, parameter);
 
 		try
 		{
-			if(command=="display" or command=="d") 				myList.display();
-			else if(command=="front" or command=="f")			cout<<"Front = "<<myList.front()<<endl;
-			else if(command=="back" or command=="b")			cout<<"Back = "<<myList.back()<<endl;
-			else if(command=="addFront" or command=="af")		myList.addFront(parameter),myList.display();
-			else if(command=="addBack"	or command=="ab")		myList.addBack(parameter),myList.display();
-			else if(command=="removeFront" or command=="rf")	myList.removeFront(),myList.display();
-			else if(command=="removeBack" or command=="rb")		myList.removeBack(),myList.display();
-			else if(command=="isEmpty"	or command=="ise")		cout<<"List is "<<(myList.isEmpty()? "Empty":"Not Empty")<<endl;
-			else if(command=="reverse"	or command=="r")		myList.reverseList(),myList.display();
-			else if(command=="isPalindrome" or command=="isp")	cout<<"List is "<<(myList.isPalindrome()? "Palindrome":"Not Palindrome")<<endl;
-			else if(command == "help" or command=="h")			listCommands();
-			else if(command == "exit" or command=="e")			break;
-			else 												cout<<"Invalid Commad !!"<<endl;
+			if (command == "display" or command == "d")
+				myList.display();
+			else if (command == "front" or command == "f")
+				cout << "Front = " << myList.front() << endl;
+			else if (command == "back" or command == "b")
+				cout << "Back = " << myList.back() << endl;
+			else if (command == "addFront" or command == "af")
+				myList.addFront(parameter), myList.display();
+			else if (command == "addBack" or command == "ab")
+				myList.addBack(parameter), myList.display();
+			else if (command == "removeFront" or command == "rf")
+				myList.removeFront(), myList.display();
+			else if (command == "removeBack" or command == "rb")
+				myList.removeBack(), myList.display();
+			else if (command == "isEmpty" or command == "ise")
+				cout << "List is " << (myList.isEmpty() ? "Empty" : "Not Empty") << endl;
+			else if (command == "reverse" or command == "r")
+				myList.reverseList(), myList.display();
+			else if (command == "isPalindrome" or command == "isp")
+				cout << "List is " << (myList.isPalindrome() ? "Palindrome" : "Not Palindrome") << endl;
+			else if (command == "help" or command == "h")
+				listCommands();
+			else if (command == "exit" or command == "e")
+				break;
+			else
+				cout << "Invalid Commad !!" << endl;
 		}
-		catch(exception &e)
+		catch (exception &e)
 		{
-			cout<<"Exception: "<<e.what()<<endl;
+			cout << "Exception: " << e.what() << endl;
 		}
 
-	}while(command!="exit" and command!="quit");
+	} while (command != "exit" and command != "quit");
 
 	return EXIT_SUCCESS;
 }
@@ -112,115 +125,123 @@ int main()
 #ifdef _WIN32
 void DoublyLinkedList::display() const
 {
-	Node *curr=head;
-	while(curr!=NULL)
+	Node *curr = head;
+	while (curr != NULL)
 	{
-		cout<<"+------";
-		curr=curr->next;
+		cout << "+------";
+		curr = curr->next;
 	}
-	cout<<"+"<<endl<<"|";
-	curr=head;
-	while(curr!=NULL)
+	cout << "+" << endl
+		 << "|";
+	curr = head;
+	while (curr != NULL)
 	{
-		if (curr == head)		cout<<" Head |";
-		else if(curr == tail)	cout<<" Tail |";
-		else 					cout<<setw(5)<<curr->elem<<" |";
-	
-		curr=curr->next;
+		if (curr == head)
+			cout << " Head |";
+		else if (curr == tail)
+			cout << " Tail |";
+		else
+			cout << setw(5) << curr->elem << " |";
+
+		curr = curr->next;
 	}
-	curr=head;
-	cout<<endl;
-	while(curr!=NULL)
+	curr = head;
+	cout << endl;
+	while (curr != NULL)
 	{
-		cout<<"+------";
-		curr=curr->next;
+		cout << "+------";
+		curr = curr->next;
 	}
-	cout<<"+"<<endl;
+	cout << "+" << endl;
 }
 //====================================================================
 #else
 void DoublyLinkedList::display() const
 {
-	Node *curr=head;
-	while(curr!=NULL)
+	Node *curr = head;
+	while (curr != NULL)
 	{
-		cout<<"╔══════╗    ";
-		curr=curr->next;
+		cout << "╔══════╗    ";
+		curr = curr->next;
 	}
-	cout<<endl<<"║";
-	curr=head;
-	while(curr!=NULL)
+	cout << endl
+		 << "║";
+	curr = head;
+	while (curr != NULL)
 	{
-		if (curr == head)		cout<<" Head ║<══>║";
-		else if(curr == tail)	cout<<" Tail ║";
-		else 					cout<<setw(6)<<curr->elem<<"║<══>║";
-	
-		curr=curr->next;
+		if (curr == head)
+			cout << " Head ║<══>║";
+		else if (curr == tail)
+			cout << " Tail ║";
+		else
+			cout << setw(6) << curr->elem << "║<══>║";
+
+		curr = curr->next;
 	}
-	curr=head;
-	cout<<endl;
-	while(curr!=NULL)
+	curr = head;
+	cout << endl;
+	while (curr != NULL)
 	{
-		cout<<"╚══════╝    ";
-		curr=curr->next;
+		cout << "╚══════╝    ";
+		curr = curr->next;
 	}
-	cout<<endl;
+	cout << endl;
 }
 #endif
-//startercode_ends
+// startercode_ends
 //===========================================================
-//Constructor to create Sentinel (dummy) nodes
-DoublyLinkedList::DoublyLinkedList ()
+// Constructor to create Sentinel (dummy) nodes
+DoublyLinkedList::DoublyLinkedList()
 {
-	//to-do
+	// to-do
 }
 //============================================================
-DoublyLinkedList::~DoublyLinkedList () 
+DoublyLinkedList::~DoublyLinkedList()
 {
-	//to-do
+	// to-do
 }
 //============================================================
-bool DoublyLinkedList::isEmpty() const 
+bool DoublyLinkedList::isEmpty() const
 {
-	//to-do
+	// to-do
 }
 //============================================================
-const string& DoublyLinkedList::front() const 
+const string &DoublyLinkedList::front() const
 {
-	//to-do
+	// to-do
 }
 //============================================================
-const string& DoublyLinkedList::back() const 
+const string &DoublyLinkedList::back() const
 {
-	//to-do
+	// to-do
 }
 //============================================================
-void DoublyLinkedList::addFront(const string& elem) 
+void DoublyLinkedList::addFront(const string &elem)
 {
-	//to-do
+	// to-do
 }
 //============================================================
-void DoublyLinkedList::addBack(const string& elem) 
+void DoublyLinkedList::addBack(const string &elem)
 {
-	//to-do
+	// to-do
 }
 //============================================================
-void DoublyLinkedList::removeFront() 
+void DoublyLinkedList::removeFront()
 {
-	//to-do
+	// to-do
 }
 //============================================================
-void DoublyLinkedList::removeBack() 
+void DoublyLinkedList::removeBack()
 {
-	//to-do
+	// to-do
 }
 //============================================================
-void DoublyLinkedList::reverseList() 
+void DoublyLinkedList::reverseList()
 {
-	//to-do
+	// to-do
 }
 //============================================================
 bool DoublyLinkedList::isPalindrome() const
 {
-	//to-do
+	// to-do
 }
